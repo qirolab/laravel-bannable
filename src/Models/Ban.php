2c9fd2649b10c30cf55fc8d71ed3455e1fd9fb23
Harish Kumar <harish.pra22@gmail.com>
2018-07-03 01:10:35 +0530

    Ban Model

---
+++ src/Models/Ban.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Hkp22\Laravel\Bannable\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Ban extends Model
+{
+    use SoftDeletes;
+
+    /**
+    * The table associated with the model.
+    *
+    * @var string
+    */
+    protected $table = 'bans';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['comment', 'expired_at'];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'expired_at' => 'datetime',
+    ];
+
+    /**
+     * Expired timestamp mutator.
+     *
+     * @param \Carbon\Carbon|string $value
+     */
+    public function setExpiredAtAttribute($value)
+    {
+        if (!$value instanceof Carbon) {
+            $value = Carbon::parse($value);
+        }
+
+        $this->attributes['expired_at'] = $value;
+    }
+
+    /**
+     * Entity responsible for ban.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\MorphTo
+     */
+    public function createdBy()
+    {
+        return $this->morphTo('created_by');
+    }
+
+    /**
+     * Bannable model.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\MorphTo
+     */
+    public function bannable()
+    {
+        return $this->morphTo('bannable');
+    }
+}
