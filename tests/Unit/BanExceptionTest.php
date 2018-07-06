a4820b1b723c776ab94e252c90f36a4dfd45fc98
Harish Kumar <harish.pra22@gmail.com>
2018-07-06 22:47:17 +0530

    Ban middleware test

---
+++ tests/Unit/BanExceptionTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Hkp22\Tests\Laravel\Bannable\Unit;
+
+use Illuminate\Http\Request;
+use Hkp22\Tests\Laravel\Bannable\TestCase;
+use Hkp22\Laravel\Bannable\Middleware\ForbidBannedUser;
+use Hkp22\Laravel\Bannable\Exceptions\BannableTraitNotUsed;
+use Hkp22\Tests\Laravel\Bannable\Stubs\Models\UserWithoutBannableTrait;
+
+class BanExceptionTest extends TestCase
+{
+    /** @test **/
+    public function it_can_throw_BannableTraitNotUsed_exception()
+    {
+        $this->expectException(BannableTraitNotUsed::class);
+
+        $user = factory(UserWithoutBannableTrait::class)->create();
+
+        $this->actingAs($user);
+
+        $request = Request::create('/test', 'GET');
+
+        $middleware = new ForbidBannedUser(auth());
+
+        $response = $middleware->handle($request, function () {});
+    }
+}
---
+++ tests/Unit/BanMiddlewareTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Hkp22\Tests\Laravel\Bannable\Unit;
+
+use Illuminate\Http\Request;
+use Hkp22\Tests\Laravel\Bannable\TestCase;
+use Hkp22\Tests\Laravel\Bannable\Stubs\Models\User;
+use Hkp22\Laravel\Bannable\Middleware\ForbidBannedUser;
+
+class BanMiddlewareTest extends TestCase
+{
+    /** @test */
+    public function it_redirected_back_to_banned_user()
+    {
+        $user = factory(User::class)->create();
+
+        $user->ban();
+
+        $this->actingAs($user->refresh());
+
+        $request = Request::create('/test', 'GET');
+
+        $middleware = new ForbidBannedUser(auth());
+
+        $response = $middleware->handle($request, function () {});
+
+        $this->assertEquals($response->getStatusCode(), 302);
+    }
+
+    /** @test */
+    public function it_cannot_redirect_to_non_banned_user()
+    {
+        $user = factory(User::class)->create();
+
+        $this->actingAs($user);
+
+        $request = Request::create('/test', 'GET');
+
+        $middleware = new ForbidBannedUser(auth());
+
+        $response = $middleware->handle($request, function () {});
+
+        $this->assertEquals($response, null);
+    }
+}
