f849e510cb558d248fa734179d9acf5240f34373
Harish Kumar <harish.pra22@gmail.com>
2018-07-06 22:46:59 +0530

    User model without bannable trait

--- tests/database/factories/UserFactory.php
+++ tests/database/factories/UserFactory.php
@@ -3,11 +3,22 @@
 
 use Faker\Generator as Faker;
 use Hkp22\Tests\Laravel\Bannable\Stubs\Models\User;
+use Hkp22\Tests\Laravel\Bannable\Stubs\Models\UserWithoutBannableTrait;
 
 /**
  * @var \Illuminate\Database\Eloquent\Factory $factory
  */
 $factory->define(User::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name,
+    ];
+});
+
+
+/**
+ * @var \Illuminate\Database\Eloquent\Factory $factory
+ */
+$factory->define(UserWithoutBannableTrait::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
     ];
