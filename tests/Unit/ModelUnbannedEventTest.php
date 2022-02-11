6743c8a980ad0fc8a8dcc182505e77b7fb607ff7
Harish Kumar <harish.pra22@gmail.com>
2022-02-11 15:18:02 +0530

    fix event test

--- tests/Unit/ModelUnbannedEventTest.php
+++ tests/Unit/ModelUnbannedEventTest.php
@@ -2,11 +2,14 @@
 
 namespace Qirolab\Tests\Laravel\Bannable\Unit;
 
+use Illuminate\Foundation\Testing\Concerns\MocksApplicationServices;
 use Qirolab\Laravel\Bannable\Events\ModelWasUnbanned;
 use Qirolab\Tests\Laravel\Bannable\TestCase;
 
 class ModelUnbannedEventTest extends TestCase
 {
+    use MocksApplicationServices;
+
     /** @test */
     public function it_can_fire_event_on_helper_call()
     {
