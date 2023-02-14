b4ba8fa460e3585446f735522cc9910b02c4c154
Harish Kumar <harish.pra22@gmail.com>
2023-02-14 21:05:17 +0530

    remove MocksApplicationServices trait

--- tests/Unit/ModelBannedEventTest.php
+++ tests/Unit/ModelBannedEventTest.php
@@ -2,32 +2,37 @@
 
 namespace Qirolab\Tests\Laravel\Bannable\Unit;
 
-use Illuminate\Foundation\Testing\Concerns\MocksApplicationServices;
+use Illuminate\Support\Facades\Event;
 use Qirolab\Laravel\Bannable\Events\ModelWasBanned;
 use Qirolab\Tests\Laravel\Bannable\Stubs\Models\User;
 use Qirolab\Tests\Laravel\Bannable\TestCase;
 
 class ModelBannedEventTest extends TestCase
 {
-    use MocksApplicationServices;
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Event::fake([ModelWasBanned::class]);
+    }
 
     /** @test */
     public function it_can_fire_event_on_helper_call()
     {
-        $this->expectsEvents(ModelWasBanned::class);
-
         $entity = $this->createUser(User::class);
 
         $entity->ban();
+
+        Event::assertDispatched(ModelWasBanned::class);
     }
 
     /** @test */
     public function it_can_fire_event_on_relation_create()
     {
-        $this->expectsEvents(ModelWasBanned::class);
-
         $entity = $this->createUser(User::class);
 
         $entity->bans()->create();
+
+        Event::assertDispatched(ModelWasBanned::class);
     }
 }
--- tests/Unit/ModelUnbannedEventTest.php
+++ tests/Unit/ModelUnbannedEventTest.php
@@ -2,31 +2,36 @@
 
 namespace Qirolab\Tests\Laravel\Bannable\Unit;
 
-use Illuminate\Foundation\Testing\Concerns\MocksApplicationServices;
+use Illuminate\Support\Facades\Event;
 use Qirolab\Laravel\Bannable\Events\ModelWasUnbanned;
 use Qirolab\Tests\Laravel\Bannable\TestCase;
 
 class ModelUnbannedEventTest extends TestCase
 {
-    use MocksApplicationServices;
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Event::fake([ModelWasUnbanned::class]);
+    }
 
     /** @test */
     public function it_can_fire_event_on_helper_call()
     {
-        $this->expectsEvents(ModelWasUnbanned::class);
-
         $ban = $this->createBan();
 
         $ban->bannable->unban();
+
+        Event::assertDispatched(ModelWasUnbanned::class);
     }
 
     /** @test */
     public function it_can_fire_event_on_relation_delete()
     {
-        $this->expectsEvents(ModelWasUnbanned::class);
-
         $ban = $this->createBan();
 
         $ban->delete();
+
+        Event::assertDispatched(ModelWasUnbanned::class);
     }
 }
